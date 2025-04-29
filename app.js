05d6147bd5f7b36112ff26d39b941630f8acb7f7
junaantoino <junauceda@gmail.com>
2025-04-29 15:55:55 +0200

    Update app.js

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ export const createApp = ({ MovieModel }) => {
   
   const PORT = process.env.PORT ?? 1234;
   
-  app.listen(PORT, () => {
-    console.log(`Server running on http://localhost:${PORT}`);
-  });
+//   app.listen(PORT, () => {
+//     console.log(`Server running on http://localhost:${PORT}`);
+//   });
+module.exports = app;
+
 }
