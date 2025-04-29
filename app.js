6caf358115ebb19086cc6004212c131defcc6e62
junaantoino <junauceda@gmail.com>
2025-04-29 15:56:40 +0200

    Revert "Update app.js"
    This reverts commit 05d6147bd5f7b36112ff26d39b941630f8acb7f7.

--- app.js
+++ app.js
@@ -12,9 +12,7 @@ export const createApp = ({ MovieModel }) => {
   
   const PORT = process.env.PORT ?? 1234;
   
-//   app.listen(PORT, () => {
-//     console.log(`Server running on http://localhost:${PORT}`);
-//   });
-module.exports = app;
-
+  app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+  });
 }
